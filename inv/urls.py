922234da2c416bbb50d304b0b934b1d34ea5a048
Rishi Namdev <namdev373@gmail.com>
2019-09-27 16:34:31 +0530

    requirements changed

--- inv/urls.py
+++ inv/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+namespace = 'inv'
 urlpatterns = [
     path('', views.view1),
+    path('login', views.login_view),
+    path('f-pass', views.fpass_view),
+    path('register', views.register_view),
+    path('404', views.view_404),
 ]
--- inv/views.py
+++ inv/views.py
@@ -2,6 +2,23 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 
+
 # Create your views here.
 def view1(request):
     return render(request, "home.html")
+
+
+def login_view(request):
+    return render(request, 'login.html')
+
+
+def register_view(request):
+    return render(request, 'register.html')
+
+
+def view_404(request):
+    return render(request, '404.html')
+
+
+def fpass_view(request):
+    return render(request, 'forgot-password.html')
